35803032a0cdd2602d686780527c35f101629d8a
Kokila <jkokila2810@gmail.com>
2025-02-23 22:58:44 +0530

    logger is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'views/user_list_view.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,5 +16,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: UserListView(),
     );
+  }
+}
+
+//logger example:
+class LogService {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 2, // Shows how many stack trace lines
+      errorMethodCount: 8,
+      lineLength: 120,
+      colors: true,
+      printEmojis: true,
+      printTime: true,
+    ),
+  );
+
+  static void info(String message) {
+    _logger.i(message);
+  }
+
+  static void debug(String message) {
+    _logger.d(message);
+  }
+
+  static void warning(String message) {
+    _logger.w(message);
+  }
+
+  static void error(String message) {
+    _logger.e(message);
   }
 }
